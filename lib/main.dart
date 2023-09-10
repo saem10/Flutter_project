c56a3a6e76b47f3f5b938a584c64eff0a8ea0e6d
saem10 <miesaemhasan@gmail.com>
2023-09-10 11:04:51 +0600

    Assignment_5_v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,66 @@
-
 import 'package:flutter/material.dart';
 
 void main() {
-  // give me some widget
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// Hot reload => ctl + s
-// Hot restart => ctl + shift + \
-
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      home: HomeScreen(),
-      title: 'Ostad app',
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: Text('Home'),
-        leading: Icon(
-          Icons.add_business,
-          color: Colors.white,
-          size: 32,
-        ),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.search, color: Colors.white, size: 32),
-            onPressed: () {},
-          )
-        ],
-      ),
-      body: Column(
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('This is mod 5 Assignment',
-                  style: TextStyle(fontSize: 25, color: Colors.black)),
-            ],
-          ),
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                    text: 'My',
-                    style: TextStyle(fontSize: 30, color: Colors.red)),
-                TextSpan(text: ' phone', style: TextStyle(color: Colors.blue)),
-                TextSpan(
-                    text: ' name',
-                    style: TextStyle(fontSize: 20, color: Colors.purple)),
-                TextSpan(
-                    text: ' is Iphone 12',
-                    style: TextStyle(fontSize: 30, color: Colors.orangeAccent)),
-              ],
+      title: 'Flutter AppBar Example',
+      home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100, // Adjust the height as needed
+          elevation: 10, // Adjust the elevation as needed
+          leading: Icon(Icons.add_business),
+          title: Text(
+            'Home',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 20, // Adjust the font size as needed
+              fontWeight: FontWeight.bold, // Optional, adjust as needed
             ),
           ),
-        ],
+          centerTitle: true, // Center the title horizontally
+          actions: [
+            Icon(Icons.search),
+          ],
+          backgroundColor: Colors.green,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('This is mod 5 Assignment'),
+              RichText(
+                text: TextSpan(
+                  style: DefaultTextStyle.of(context).style,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: 'My ',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                    TextSpan(
+                      text: 'phone ',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                    TextSpan(
+                      text: 'name ',
+                      style: TextStyle(color: Colors.purple),
+                    ),
+                    TextSpan(
+                      text: 'Your phone name',
+                      style: TextStyle(color: Colors.orange),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
