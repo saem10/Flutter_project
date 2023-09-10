0ef93a1d9efa4f7464bc8c44ca7321bc873925de
saem10 <miesaemhasan@gmail.com>
2023-09-10 15:19:52 +0600

    appBar,Icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,40 +2,50 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    //  App entry point
-    //  display kore sob function
-
-   /*   MaterialApp(
-        // widget jar mode onek function ace
-        // ja dia app Interior design kora jay
-           home: Scaffold(
-        // Scaffold widget is part of the Flutter framework
-        // and is typically used as the top-level widget in a Flutter app.
-        // AppBar,body,FloatingActionButton
-           body: Center(
-             child:Text('Hello world') ,
-                ),
-               ),
-              ),
-   */
-
       MyApp()
-
-  ); // RunApp
+  );
 }
+
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
    return MaterialApp(
      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Home'),
-        ),
-        body: Center(
-          child: Text('Hello screen'),
-      ),
-     ),
+      home: HomeScreen(),
    );
+  }
+}
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+        leading: Icon(Icons.add_business,size: 30,color: Colors.black),
+        title: Text('Home'),
+      ),
+      body:Column(
+        children: [
+          Text("Mir Saem Hasan",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          Text("Mir Saem Hasan",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 40,
+                fontWeight: FontWeight.bold
+            ),
+          ),
+          Icon(Icons.account_circle,size: 100,color: Colors.tealAccent,),
+        ],
+      )
+    );
   }
 }
