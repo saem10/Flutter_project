98880623815f0b6b31e256e9422ef7c8e76117cc
saem10 <miesaemhasan@gmail.com>
2023-09-10 15:51:32 +0600

    practice-1

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,10 @@ void main() {
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
-   return MaterialApp(
-     debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
-   );
+    );
   }
 }
 
@@ -20,32 +20,97 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-        leading: Icon(Icons.add_business,size: 30,color: Colors.black),
-        title: Text('Home'),
-      ),
-      body:Column(
-        children: [
-          Text("Mir Saem Hasan",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.bold
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: Text('Home'),
+          leading: Icon(Icons.add_business,size: 30,color: Colors.black),
+
+
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search,size: 30,color: Colors.black), // Icon on the right
+              onPressed: () {
+                // Handle the right icon's onPressed event here
+              },
             ),
-          ),
-          Text("Mir Saem Hasan",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 40,
-                fontWeight: FontWeight.bold
+            IconButton(
+              icon: Icon(Icons.settings,size: 30,color: Colors.black), // Another icon on the right
+              onPressed: () {
+                // Handle the right icon's onPressed event here
+              },
             ),
-          ),
-          Icon(Icons.account_circle,size: 100,color: Colors.tealAccent,),
-        ],
-      )
+          ],
+
+
+
+        ),
+        body:Column(
+          children: [
+            Text("Mir Saem Hasan",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Text("Mir Saem Hasan",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+            Icon(Icons.account_circle,size: 100,color: Colors.tealAccent,),
+          ],
+        )
     );
   }
 }
+/*
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+          leading: IconButton(
+            icon: Icon(Icons.menu), // Icon on the left
+            onPressed: () {
+              // Handle the left icon's onPressed event here
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search), // Icon on the right
+              onPressed: () {
+                // Handle the right icon's onPressed event here
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.settings), // Another icon on the right
+              onPressed: () {
+                // Handle the right icon's onPressed event here
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          child: Text('Hello, World!'),
+        ),
+      ),
+    );
+  }
+}
+
+
+ */
