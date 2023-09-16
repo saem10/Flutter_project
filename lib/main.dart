53230b4f4b9f0cd9a7fe118eb307f8b2048e5af5
saem10 <miesaemhasan@gmail.com>
2023-09-16 21:40:12 +0600

    listView_builder, listview_separated

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
 void main(){
   runApp(
-    MyApp()
+      MyApp()
   );
 }
 class MyApp extends StatelessWidget{
@@ -17,32 +16,40 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Home'),
-      ),
-
-      body:
-       Scrollbar(
-         thickness: 10,
-         radius: Radius.circular(8),
-                   child: GridView.builder(
-                     itemCount: 100,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4),
-                     itemBuilder: (BuildContext context, int index) {
-                      return Column(
-                        children: [
-                          Text(index.toString()),
-                          Icon(Icons.add_business),
-                        ],
-                      );
-                     },
-
-                   ),
-
-       ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('Home'),
+        ),
+        body:
+        Scrollbar(
+          thickness: 10,
+          radius: Radius.circular(8),
+          child:
+          /*ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('item number $index'),
+              );
+             }),*/
+          ListView.separated(
+            itemCount: 20,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('item number $index'),
+              );
+            },
+            separatorBuilder: (context,index){
+              return Column(
+                children: [
+                  Text(index.toString()),
+                  Divider(),
+                ],
+              );
+            },
+          ),
+        )
     );
   }
 }
