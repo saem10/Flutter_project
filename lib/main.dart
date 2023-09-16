68572617649100ae7e7db1423b109f17b508dca1
saem10 <miesaemhasan@gmail.com>
2023-09-16 23:33:16 +0600

    module-6(quiz-1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,61 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(
-      MyApp()
-  );
+
+void main() {
+  runApp(MyApp());
 }
-class MyApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-class HomeScreen extends StatefulWidget{
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<String> students = [
-    'Saem',
-    'Hasan',
-    'Arup',
-    'Kazi',
-    'Faruk',
-    'Saem',
-    'Hasan',
-    'Arup',
-    'Kazi',
-    'Faruk',
-  ];
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        backgroundColor: Colors.white,
+      home: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text('Home'),
-        ),
-        body:
-        Scrollbar(
-          thickness: 10,
-          radius: Radius.circular(8),
-          child:ListView.separated(
-            itemCount: students.length,
-            itemBuilder: (context,index){
-            return ListTile(
-              onTap: (){
-                print('go to details screen');
+          title: Text("My Shopping List"),
+          centerTitle: true, // Center the title in the app bar
+          actions: [
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {
+                // Add cart functionality here
               },
-              title:Text(students[index]),
-            );
-           },
-            separatorBuilder: (context,index){
-              return Divider();
-            },
-          ),
-
-
-        )
+            ),
+          ],
+        ),
+        body: ShoppingList(),
+      ),
     );
   }
 }
 
+class ShoppingList extends StatelessWidget {
+  final List<Map<String, String>> items = [
+    {"name": "Apples", "icon": "🍎"},
+    {"name": "Bananas", "icon": "🍌"},
+    {"name": "Milk", "icon": "🥛"},
+    {"name": "Bread", "icon": "🍞"},
+    {"name": "Eggs", "icon": "🥚"},
+  ];
 
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (BuildContext context, int index) {
+        final item = items[index];
+        return ListTile(
+          leading: Text(item['icon'] ?? '', style: TextStyle(fontSize: 24.0)),
+          title: Text(item['name'] ?? ''),
+          onTap: () {
+            // Add functionality when an item is tapped.
+            // For example, you can show a snackbar message.
+           /* ScaffoldMessenger.of(context).showSnackBar(
+               SnackBar(
+                content: Text("You tapped ${item['name']}"),
+              ),
+            );*/
+          },
+        );
+      },
+    );
+  }
+}
