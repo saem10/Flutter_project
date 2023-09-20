cedd77a576a18e359fd97415066e062d69c1a7a7
saem10 <miesaemhasan@gmail.com>
2023-09-20 07:25:56 +0600

    Basic data passing one route to another route has been added

--- lib/main.dart
+++ lib/main.dart
@@ -1,153 +1,112 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(PhotoGalleryApp());
+  runApp(Myapp());
 }
 
-class PhotoGalleryApp extends StatelessWidget {
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("Photo Gallery")), // Center the app bar title
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+///Route
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Home',
+              style: TextStyle(fontSize: 40),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => ProductListTitle()));
+              },
+              child: Text('Go to product list'),
+            ),
+          ],
         ),
-        body: PhotoGallery(),
+      ),
+    );
+  }
+}
+//Route
+class ProductListTitle extends StatelessWidget {
+  const ProductListTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product list'),
+      ),
+      body: ListView.builder(
+        itemCount: 50,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) =>
+                      ProductDetailsScreen(productName: index.toString(),),
+                ),
+              ).then((value) {
+                print(value);
+              });
+            },
+            title: Text(index.toString()),
+          );
+        },
       ),
     );
   }
 }
 
-class PhotoGallery extends StatelessWidget {
-  final List<String> imageUrls = [
-    "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg", // Add your image URLs here
-    "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg", // Add your image URLs here
-    "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg", // Add your image URLs here
-    "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg", // Add your image URLs here
+class ProductDetailsScreen extends StatelessWidget {
+  final String productName;/// Mandatory
+  final double? price;/// Optional
 
-  ];
+  const ProductDetailsScreen(
+      {super.key, required this.productName, this.price});
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child:
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Text(
-              "Welcome to our Photo Gallery!",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextField(
-              decoration: InputDecoration(
-                labelText: "Search For Photos...",
-                border: OutlineInputBorder(),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text("Grid tapped!"),
-                ),
-              );
-            },
-            child: Wrap(
-              alignment: WrapAlignment.center,
-              children: imageUrls.asMap().entries.map((entry) {
-                final int index = entry.key;
-                final String url = entry.value;
-                return GestureDetector(
-                  onTap: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Image $index clicked!"), // Rename the image captions
-                      ),
-                    );
-                  },
-                  child: Card(
-                    child: Column(
-                      children: <Widget>[
-                        Image.network(
-                          url,
-                          height: 100,
-                          width: 150,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Text("Image $index"), // Use "Image $index" as the caption
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ),
-          ListTile(
-            leading: Image.network(
-              // Replace with your sample image URL
-              "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg",
-              height: 60,
-              width: 60,
-              fit: BoxFit.cover,
-            ),
-            title: Text("Photo 1"),
-            subtitle: Text("Description for photo 1"),
-          ),
-          ListTile(
-            leading: Image.network(
-              // Replace with your sample image URL
-              "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg",
-              height: 60,
-              width: 60,
-              fit: BoxFit.cover,
-            ),
-            title: Text("Photo 2"),
-            subtitle: Text("Description for photo 2"),
-          ),
-          ListTile(
-            leading: Image.network(
-              // Replace with your sample image URL
-              "https://www.dreamhost.com/blog/wp-content/uploads/2019/06/afa314e6-1ae4-46c5-949e-c0a77f042e4f_DreamHost-howto-prod-descrips-full.jpeg",
-              height: 60,
-              width: 60,
-              fit: BoxFit.cover,
-            ),
-            title: Text("Photo 3"),
-            subtitle: Text("Description for photo 3"),
-          ),
-          Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(200),
-            ),
-            child: IconButton(
-              icon: Icon(
-                Icons.upload,
-                color: Colors.white,
-              ),
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    content: Text("Photos Uploaded Successfully!"),
-                  ),
-                );
-              },
-            ),
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Details'),
       ),
-
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              productName,
+              style: TextStyle(fontSize: 30),
+            ),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context, 'My name $productName');
+            }, child: Text('Back'),)
+          ],
+        ),
+      ),
     );
   }
 }
