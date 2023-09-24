bbf30f40ff55b946615c350ee5d429c4eca5751b
saem10 <miesaemhasan@gmail.com>
2023-09-24 21:00:26 +0600

    stateful widget, counting number application

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,115 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Myapp());
+void main() {
+  runApp(MyApp());
 }
-class Myapp extends StatelessWidget {
-   Myapp({super.key});
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      home: CounterScreen(),
     );
   }
 }
-class Home extends StatelessWidget {
-  const Home({super.key});
 
+/*class Home extends StatelessWidget {
+  Home({super.key});
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Column(
-        children: [
-          Switch(value: true, onChanged: (bool value){}),
-          ElevatedButton(onPressed: (){
-            showDialog(
-              barrierDismissible: false,
-              context: context, builder: (context)=>AlertDialog(
-              title: Text('Alert'),
-              content: Text('You are in danger'),
-              actions: [
-                TextButton(onPressed: (){
-                  Navigator.pop(context);
-                }, child: Text('Cancle')),
-                TextButton(onPressed: (){
-                  // Action
-                }, child: Text('ok')),
-              ],
+      body: Center(
+        child: Text(
+        counter.toString(),
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter = counter + 1 ;
+          print(counter);
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}*/
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterState();
+}
+
+class _CounterState extends State<CounterScreen> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              counter.toString(),
+              style: Theme.of(context).textTheme.displayLarge,
             ),
-            );
-          }, child: Text('Show dialog'),),
-          ElevatedButton(
-              onPressed: () {
-                showAboutDialog(
-                  context: context,
-                  applicationName: 'Chete',
-                  applicationVersion: '0.0.1',
-                );
-              },
-              child: Text('show about')),
-          ElevatedButton(onPressed: (){
-            showModalBottomSheet(
-                isDismissible: true,
-                barrierColor: Colors.blueGrey ,
-                backgroundColor: Colors.grey.shade100,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(12),
-                    topLeft: Radius.circular(12),
-                  )
-                ),
-                context: context, builder: (context){
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  Center(
-                    child: Text('This is bottom sheet'),
-                  ),
-                ],),
-              );
-            });
-          }, child: Text('show bottom sheet'))
-        ],
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => ProfileScreen(userName: "Tultul"),
+                    ),
+                  );
+                },
+                child: Text("Profile"))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter = counter + 1 ;
+          print(counter);
+          setState(() {});
+        },
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
+// Widger class
+class ProfileScreen extends StatefulWidget {
+  final String userName;
+  const ProfileScreen({super.key, required this.userName});
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+// state class
+// state class -> widget class
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body:Center(
+        child: Text(widget.userName ),
+      )
+
+
+    );
+  }
+}
