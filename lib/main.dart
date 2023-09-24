ebed4b222442a8a3fc57b31e1168755ae3a99bcf
saem10 <miesaemhasan@gmail.com>
2023-09-24 15:11:30 +0600

    show Dialog button

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,81 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(Myapp());
 }
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class Myapp extends StatelessWidget {
+   Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoApp(
+    return MaterialApp(
       home: Home(),
     );
   }
 }
-
 class Home extends StatelessWidget {
-  Home({super.key});
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        leading: Icon(CupertinoIcons.home),
-        middle: Text('Home'),
-        trailing: CupertinoSwitch(
-          onChanged: (onChange) {},
-          value: true,
-        ),
-        backgroundColor: Colors.green,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
       ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Saem Hasan'),
-            Text('Saem Hasan'),
-            Text('Saem Hasan'),
-            Text('Saem Hasan'),
-            Text('Saem Hasan'),
-            Text('Saem Hasan'),
-            CupertinoButton(child: Text('Saem'), onPressed: (){} ),
-            CupertinoButton.filled(child: Text('Saem'), onPressed: (){} ),
-            CupertinoTextField(),
-            CupertinoTabBar(items: [
-              BottomNavigationBarItem(label:'Mir' ,icon: Icon(CupertinoIcons.home)),
-              BottomNavigationBarItem(label: 'Saem',icon: Icon(CupertinoIcons.home)),
-              BottomNavigationBarItem(label: 'Hasan',icon: Icon(CupertinoIcons.home)),
-            ])
-            
-          ],
-        ),
+      body: Column(
+        children: [
+          Switch(value: true, onChanged: (bool value){}),
+          ElevatedButton(onPressed: (){
+            showDialog(
+              barrierDismissible: false,
+              context: context, builder: (context)=>AlertDialog(
+              title: Text('Alert'),
+              content: Text('You are in danger'),
+              actions: [
+                TextButton(onPressed: (){
+                  Navigator.pop(context);
+                }, child: Text('Cancle')),
+                TextButton(onPressed: (){
+                  // Action
+                }, child: Text('ok')),
+              ],
+            ),
+            );
+          }, child: Text('Show dialog'),),
+          ElevatedButton(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  applicationName: 'Chete',
+                  applicationVersion: '0.0.1',
+                );
+              },
+              child: Text('show about')),
+          ElevatedButton(onPressed: (){
+            showModalBottomSheet(
+                isDismissible: true,
+                barrierColor: Colors.blueGrey ,
+                backgroundColor: Colors.grey.shade100,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(12),
+                    topLeft: Radius.circular(12),
+                  )
+                ),
+                context: context, builder: (context){
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  Center(
+                    child: Text('This is bottom sheet'),
+                  ),
+                ],),
+              );
+            });
+          }, child: Text('show bottom sheet'))
+        ],
       ),
     );
   }
 }
+
