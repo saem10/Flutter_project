11380c4ac05892def2e9ab05a2ac574b707e1bc4
Mir Saem Hasan <mirsaemhasan@gmail.com>
2023-09-30 19:06:39 +0600

    Assignment8

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,182 @@
-import 'package:application_2/home_screen.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+
+        return const MaterialApp(
+          title: 'Flutter Demo',
+          home: HomePage(),
+        );
+
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myItems = [
+    {
+      "img": "images/mountain.jpg",
+    },
+    {
+      "img": "images/mountain.jpg",
+    },
+    {
+      "img": "images/mountain.jpg",
+    },
+    {
+      "img": "images/mountain.jpg",
+    },
+    {
+      "img": "images/mountain.jpg",
+    },
+    {
+      "img": "images/mountain.jpg",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:HomeScreen(),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Profile"),
+      ),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          final isLandscape = orientation == Orientation.landscape;
+          final crossAxisCount = isLandscape ? 3 : 3;
+
+          return isLandscape
+              ? Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ClipOval(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.width,
+                    child: Image.asset('images/mountain.jpg',
+                        fit: BoxFit.cover),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(
+                        "John Doe",
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                      child: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo tincidunt augue Contrary to popular belief, simply random text.'),
+                    ),
+                    Expanded(
+                      child: GridView.builder(
+                        gridDelegate:
+                        SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: crossAxisCount,
+                          crossAxisSpacing: 0.8,
+                          childAspectRatio: isLandscape ? 1.1 : .8,
+                        ),
+                        itemCount: myItems.length,
+                        itemBuilder: (context, index) {
+                          return GestureDetector(
+                            child: Container(
+                              margin: const EdgeInsets.all(5),
+                              width: double.infinity,
+                              height: isLandscape ? 100 : 150,
+                              child: Image.asset(
+                                myItems[index]['img']!,
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )
+              : Column(
+            children: [
+              ClipOval(
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width,
+                  child:
+                  Image.asset('images/mountain.jpg', fit: BoxFit.cover),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  "John Doe",
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+                child: Text(
+                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent commodo tincidunt augue Contrary to popular belief, simply random text.'),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: crossAxisCount,
+                    crossAxisSpacing: 0.8,
+                    childAspectRatio: isLandscape ? 0.9 : 1.1,
+                  ),
+                  itemCount: myItems.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      child: Container(
+                        margin: const EdgeInsets.all(5),
+                        width: double.infinity,
+                        height: isLandscape ? 100 : 150,
+                        child: Image.asset(
+                          myItems[index]['img']!,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
+      ),
     );
   }
 }
