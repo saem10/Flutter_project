1515cc36c5338e1f325052985b4d97e6d1b962d4
Mir Saem Hasan <mirsaemhasan@gmail.com>
2023-10-01 18:52:36 +0600

    DevicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
 import 'package:application_2/home_screen.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+void main() {
+  runApp(
+    DevicePreview(
+      //enabled: true,
+      enabled: kDebugMode,
+      builder: (context) {
+        return MyApp();
+      },
+    ),
+  );
 }
+
 class MyApp extends StatelessWidget {
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomeScreen(),
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      home: HomeScreen(),
     );
   }
 }
