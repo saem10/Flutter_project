830b8787ce42499fe6cd907403a0c081a0fb29c1
Mir Saem Hasan <mirsaemhasan@gmail.com>
2023-10-13 23:42:22 +0600

    10-Live Quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,71 +1,103 @@
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button Color Change',
-      home: ButtonColorChangeScreen(),
+      title: 'Selection Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ItemSelectorScreen(),
     );
   }
 }
 
-class ButtonColorChangeScreen extends StatefulWidget {
-  const ButtonColorChangeScreen({super.key});
-
+class ItemSelectorScreen extends StatefulWidget {
   @override
-  _ButtonColorChangeScreenState createState() => _ButtonColorChangeScreenState();
+  _ItemSelectorScreenState createState() => _ItemSelectorScreenState();
 }
 
-class _ButtonColorChangeScreenState extends State<ButtonColorChangeScreen> {
-  MySnackBar(message, context) {
-    return ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
-  }
-  List<String> buttonSizes = ['S', 'M', 'L', 'XL','XXL', 'XXXL'];
-  String? selectedSize;
+class _ItemSelectorScreenState extends State<ItemSelectorScreen> {
+  List<Item> items = [
+    Item("Item 1", false),
+    Item("Item 2", false),
+    Item("Item 3", false),
+    Item("Item 4", false),
+    Item("Item 5", false),
+  ];
 
-  void handleButtonTap(String size) {
+  void toggleItem(int index) {
     setState(() {
-      selectedSize = size;
+      items[index].isSelected = !items[index].isSelected;
     });
+  }
 
+  void showSelectedItemsDialog() {
+    int selectedCount = items.where((item) => item.isSelected).length;
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Selected Items'),
+          content: Text('The Number of  Selected Item : $selectedCount '),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Size Selector'),
-        centerTitle: true,
+        title: Text('Selection Screen'),
       ),
-      body: Center(
-        child: GridView.count(
-          shrinkWrap: true,
-          crossAxisCount: 4, // Change the number of columns as needed
-          padding: const EdgeInsets.all(16.0),
-          mainAxisSpacing: 16.0,
-          crossAxisSpacing: 16.0,
-          children: buttonSizes.map((size) {
-            return ElevatedButton(
-              onPressed: () {
-                MySnackBar("You have pressed size: $size", context);
-                return handleButtonTap(size);
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: size == selectedSize ? Colors.orange : Colors.grey, // Change color based on selection
-
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(
+              items[index].name,
+              style: TextStyle(
+                color: items[index].isSelected ? Colors.blue[900] : Colors.black,
               ),
-              child: Text(size),
-            );
-          }).toList(),
-        ),
+            ),
+            onTap: () {
+              toggleItem(index);
+            },
+            selected: items[index].isSelected,
+            selectedTileColor: Colors.blue.withOpacity(0.2),
+          );
+        },
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showSelectedItemsDialog();
+        },
+        child: Icon(Icons.check,color: Colors.white,),
+        backgroundColor: Colors.blue,
+        elevation: 2.0,
+        shape: CircleBorder(),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
+}
+
+class Item {
+  String name;
+  bool isSelected;
+
+  Item(this.name, this.isSelected);
 }
